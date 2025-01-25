4ab94da174c9255c1ddb70e084e9fde94ea63ea5
khennyboy <abdullateefkehinde848@gmail.com>
2025-01-25 15:48:18 +0100

    done

--- src/components/memories.jsx
+++ src/components/memories.jsx
@@ -21,31 +21,31 @@ import { SectionsRef } from "../App";
 export default function GallerySection() {
   const {galleryRef} = useContext(SectionsRef)
   const images = [
-    { largeURL: image1, thumbnailURL: image1, width: 1200, height: 800 },
-    { largeURL: image2, thumbnailURL: image2, width: 1200, height: 800 },
-    { largeURL: image3, thumbnailURL: image3, width: 1200, height: 800 },
-    { largeURL: image4, thumbnailURL: image4, width: 1200, height: 800 },
-    { largeURL: image5, thumbnailURL: image5, width: 1200, height: 800 },
-    { largeURL: image6, thumbnailURL: image6, width: 1200, height: 800 },
-    { largeURL: image7, thumbnailURL: image7, width: 1200, height: 800 },
-    { largeURL: image8, thumbnailURL: image8, width: 1200, height: 800 },
-    { largeURL: image9, thumbnailURL: image9, width: 1200, height: 800 },
-    { largeURL: image10, thumbnailURL: image10, width: 1200, height: 800 },
-    { largeURL: image11, thumbnailURL: image11, width: 1200, height: 800 },
-    { largeURL: image12, thumbnailURL: image12, width: 1200, height: 800 },
-    { largeURL: image13, thumbnailURL: image13, width: 1200, height: 800 },
-    { largeURL: image14, thumbnailURL: image14, width: 1200, height: 800 },
-    { largeURL: image15, thumbnailURL: image15, width: 1200, height: 800 },
+    { largeURL: image1, thumbnailURL: image1,  },
+    { largeURL: image2, thumbnailURL: image2,  },
+    { largeURL: image3, thumbnailURL: image3,  },
+    { largeURL: image4, thumbnailURL: image4,  },
+    { largeURL: image5, thumbnailURL: image5,  },
+    { largeURL: image6, thumbnailURL: image6,  },
+    { largeURL: image7, thumbnailURL: image7,  },
+    { largeURL: image8, thumbnailURL: image8,  },
+    { largeURL: image9, thumbnailURL: image9,  },
+    { largeURL: image10, thumbnailURL: image10,  },
+    { largeURL: image11, thumbnailURL: image11,  },
+    { largeURL: image12, thumbnailURL: image12,  },
+    { largeURL: image13, thumbnailURL: image13,  },
+    { largeURL: image14, thumbnailURL: image14,  },
+    { largeURL: image15, thumbnailURL: image15,  },
   ];
 
   useEffect(() => {
     let lightbox = new PhotoSwipeLightbox({
-      // arrowPrevSVG: leftArrowSVGString,
-      // arrowNextSVG: leftArrowSVGString,
      
+    
       gallery: "#gallery",
       children: "a",
       pswpModule: () => import("photoswipe"),
+      mainClass: "pswp--custom-image-width",
     });
     lightbox.init();
 
@@ -64,12 +64,10 @@ export default function GallerySection() {
         A collection of cherished moments and unforgettable memories. Enjoy!
       </p>
 
-      <div className="pswp-gallery mt-8 grid grid-cols-2 gap-x-4 gap-y-8 md:gap-x12  md:gap-y-16 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 ">
+      <div className="pswp-gallery mt-8 grid grid-cols-2 gap-x-4 gap-y-8 md:gap-x12  md:gap-y-16 sm:grid-cols-3 lg:grid-cols-4 :grid-cols-5 ">
         {images.map((image, index) => (
           <a
             href={image.largeURL}
-            data-pswp-width={image.width}
-            data-pswp-height={image.height}
             key={`gallery-${index}`}
             target="_blank"
             rel="noreferrer"
